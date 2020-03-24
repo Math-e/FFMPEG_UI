301e63116e364bd24eaf940c3731acc8df0d0d37
Math-e <32374265+Math-e@users.noreply.github.com>
2020-03-24 19:10:55 -0300

    first push

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace FFMPEG2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textFile = new System.Windows.Forms.TextBox();
+            this.textResX = new System.Windows.Forms.TextBox();
+            this.textResY = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.textStartT = new System.Windows.Forms.TextBox();
+            this.textEndT = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textFile
+            // 
+            this.textFile.Location = new System.Drawing.Point(12, 69);
+            this.textFile.Name = "textFile";
+            this.textFile.ReadOnly = true;
+            this.textFile.Size = new System.Drawing.Size(270, 20);
+            this.textFile.TabIndex = 1;
+            this.textFile.TextChanged += new System.EventHandler(this.textFile_TextChanged);
+            // 
+            // textResX
+            // 
+            this.textResX.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textResX.Location = new System.Drawing.Point(138, 6);
+            this.textResX.MaxLength = 5;
+            this.textResX.Name = "textResX";
+            this.textResX.Size = new System.Drawing.Size(126, 31);
+            this.textResX.TabIndex = 2;
+            this.textResX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.number_filter);
+            // 
+            // textResY
+            // 
+            this.textResY.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.textResY.Location = new System.Drawing.Point(138, 42);
+            this.textResY.MaxLength = 5;
+            this.textResY.Name = "textResY";
+            this.textResY.Size = new System.Drawing.Size(126, 31);
+            this.textResY.TabIndex = 3;
+            this.textResY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.number_filter);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(270, 50);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Convert video";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 343);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(270, 50);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Select folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select a video";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.textResX, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textResY, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 124);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(270, 78);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.textBox2.Location = new System.Drawing.Point(6, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox2.Size = new System.Drawing.Size(126, 24);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Size Y";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.textBox1.Location = new System.Drawing.Point(6, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(126, 24);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Size X";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.textStartT, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textEndT, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.textBox5, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.textBox6, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 217);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.Padding = new System.Windows.Forms.Padding(3);
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(270, 78);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // textStartT
+            // 
+            this.textStartT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textStartT.Location = new System.Drawing.Point(138, 6);
+            this.textStartT.MaxLength = 5;
+            this.textStartT.Name = "textStartT";
+            this.textStartT.Size = new System.Drawing.Size(126, 31);
+            this.textStartT.TabIndex = 2;
+            // 
+            // textEndT
+            // 
+            this.textEndT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.textEndT.Location = new System.Drawing.Point(138, 42);
+            this.textEndT.MaxLength = 5;
+            this.textEndT.Name = "textEndT";
+            this.textEndT.Size = new System.Drawing.Size(126, 31);
+            this.textEndT.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Enabled = false;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.textBox5.Location = new System.Drawing.Point(6, 42);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox5.Size = new System.Drawing.Size(126, 24);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.Text = "End time";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Enabled = false;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.textBox6.Location = new System.Drawing.Point(6, 6);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox6.Size = new System.Drawing.Size(126, 24);
+            this.textBox6.TabIndex = 4;
+            this.textBox6.Text = "Start time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.textFile);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 500);
+            this.MinimumSize = new System.Drawing.Size(600, 500);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "FFMPEG-UI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textFile;
+        private System.Windows.Forms.TextBox textResX;
+        private System.Windows.Forms.TextBox textResY;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox textStartT;
+        private System.Windows.Forms.TextBox textEndT;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FFMPEG2
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFile1;
+        private SaveFileDialog saveFile1;
+        public Form1()
+        {
+            openFile1 = new OpenFileDialog();
+            saveFile1 = new SaveFileDialog {
+                AddExtension = true,
+                DefaultExt = ".mp4",
+                Filter = "Video Files | *.mp4;*.mkv;*.wmv;*.webm",
+                FilterIndex = 1,
+            };
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e){ //select file
+            if (openFile1.ShowDialog() == DialogResult.OK){
+                try{
+                    var sr = openFile1.FileName;
+                    SetText(sr);
+                    button3.Enabled = true;
+                }
+                catch (SecurityException ex){
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e){ //convert file
+            if (textFile.Text != "" && openFile1.FileName != ""){
+
+                string command1 = "/C ffmpeg " + openFile1.FileName;
+
+                if (textStartT.Text != "") {
+                    command1 += "-ss " + textStartT.Text;
+                }
+                if(textEndT.Text != "") {
+                    command1 += " -to " + textEndT.Text;
+                }
+
+
+                command1 += " -i ";
+
+                if (textResX.Text != "" && textResY.Text != "") {
+                    command1 += " -s " + textResX.Text + "x" + textResY.Text;
+                }
+                command1 += " " + saveFile1.FileName;
+
+                System.Diagnostics.Process.Start("CMD.exe",command1);
+
+            } else {
+                SetText("No file selected!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) { //select save file
+            if (saveFile1.ShowDialog() == DialogResult.OK) {
+                SetText("Save ok: " + saveFile1.FileName);
+                button2.Enabled = true;
+            }
+        }
+
+
+
+
+        private void number_filter(object sender, KeyPressEventArgs e) {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) {
+                e.Handled = true;
+            }
+        }
+        private void SetText(string text) {
+            textFile.Text = text;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e){
+
+        }
+
+        private void textFile_TextChanged(object sender, EventArgs e){
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e) {
+
+        }
+    }
+
+    
+}
